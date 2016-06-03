515b5384219bf9e3879b1f2e932f2716ae148d8f
Warwick <warwick@feedmymedia.com>
2016-06-03 12:48:46 +0200

    Prepped the 3 urls needed to connect to WETU

--- lsx-tour-importer.php
+++ lsx-tour-importer.php
@@ -51,6 +51,42 @@ class Lsx_Tour_Importer {
 	 * @var      string
 	 */
 	public $options = false;	
+
+	/**
+	 * The url to list items from WETU
+	 *
+	 * @since 0.0.1
+	 *
+	 * @var      string
+	 */
+	public $list_url = false;	
+
+	/**
+	 * The url to import items from WETU
+	 *
+	 * @since 0.0.1
+	 *
+	 * @var      string
+	 */
+	public $import_url = false;	
+
+	/**
+	 * The url to import images from WETU
+	 *
+	 * @since 0.0.1
+	 *
+	 * @var      string
+	 */
+	public $import_scaling_url = false;		
+
+	/**
+	 * scale the images on import or not
+	 *
+	 * @since 0.0.1
+	 *
+	 * @var      boolean
+	 */
+	public $scale_images = false;
 	
 	/**
 	 * Initialize the plugin by setting localization, filters, and administration functions.
@@ -61,7 +97,11 @@ class Lsx_Tour_Importer {
 	 */
 	private function __construct() {
 		register_activation_hook( __FILE__, array( $this,'plugin_activate') );
-		$this->options = get_option('_lsx_lsx-tour-importer',false);
+		$temp_options = get_option('_lsx_lsx-settings',false);
+		if(false !== $temp_options && isset($temp_options[$this->plugin_slug]) && !empty($temp_options[$this->plugin_slug])){
+			$this->options = $temp_options[$this->plugin_slug];
+			$this->set_variables();
+		}
 
 		add_filter( 'lsx_framework_settings_tabs', array( $this, 'settings_page_array') );
 		add_action( 'admin_menu', array( $this, 'register_importer_page' ) );
@@ -88,6 +128,34 @@ class Lsx_Tour_Importer {
 	public function plugin_activate() {
 		//TODO  if the lsx-tour-operator plugin is not active then do not activate.
 	}
+
+	/**
+	 * Sets the variables used throughout the plugin.
+	 */
+	public function set_variables() {
+		if(isset($this->options['token'])){
+			$this->list_url = 'http://wetu.com/API/Pins/'.$this->options['token'].'/List';
+			$this->import_url = 'http://wetu.com/API/Pins/'.$this->options['token'].'/Get?ids=';
+
+			if(isset($this->options['image_scaling'])){
+
+				$this->scale_images = true;
+				$width = '800';
+				if(isset($this->options['width']) && '' !== $this->options['width']){
+					$width = $this->options['width'];
+				}
+				$height = '600';
+				if(isset($this->options['height']) && '' !== $this->options['height']){
+					$height = $this->options['height'];
+				}
+				$cropping = 'raw';
+				if(isset($this->options['cropping']) && '' !== $this->options['cropping']){
+					$cropping = $this->options['cropping'];
+				}				
+				$this->image_scaling_url = 'https://wetu.com/ImageHandler/'.$cropping.$width.'x'.$height.'/';
+			}	
+		}
+	}	
 
 	/**
 	 * Returns the array of settings to the UIX Class in the lsx framework
@@ -124,7 +192,11 @@ class Lsx_Tour_Importer {
         <div class="wrap">
             <?php screen_icon(); ?>
             <form method="post" action="tools.php">
-            	helllo
+            	<input type="hidden" name="page" value="<?php $this->plugin_slug; ?>" />
+
+            	<?php echo $this->image_scaling_url; ?><br />
+            	<?php echo $this->list_url; ?><br />
+            	<?php echo $this->import_url; ?><br />
             </form>
         </div>
         <?php		
--- settings/lsx-tour-importer.php
+++ settings/lsx-tour-importer.php
@@ -12,23 +12,6 @@
 					<input type="text"  {{#if token}} value="{{token}}" {{/if}} name="token" />
 				</td>
 			</tr>				
-			<tr class="form-field -wrap">
-				<th scope="row">
-					<label for="username"> Username</label>
-				</th>
-				<td>
-					<input type="text"  {{#if username}} value="{{username}}" {{/if}} name="username" />
-				</td>
-			</tr>	
-			<tr class="form-field -wrap">
-				<th scope="row">
-					<label for="password"> Password</label>
-				</th>
-				<td>
-					<input type="text"  {{#if password}} value="{{password}}" {{/if}} name="password" />
-				</td>
-			</tr>			
-
 			<tr class="form-field banner-wrap">
 				<th scope="row" colspan="2"><label><h3>Image Settings</h3></label></th>
 			</tr> 
@@ -55,7 +38,21 @@
 				<td>
 					<input placeholder="600" type="text"  {{#if height}} value="{{height}}" {{/if}} name="height" />
 				</td>
-			</tr>	
+			</tr>
+
+			<tr class="form-field -wrap">
+				<th scope="row">
+					<label for="password"> Scaling</label>
+				</th>
+				<td>
+					<input type="radio" name="scaling[]" value="raw" /> Get the Full size image, no cropping takes place <br />
+					<input type="radio" name="scaling[]"  value="c" /> Crop image to fit fully into the frame, Crop is taken from middle, preserving as much of the image as possible.<br />
+					<input type="radio" name="scaling[]"  value="h" /> Crop image to fit fully into the frame, but resize to height first, then crop on width if needed<br />
+					<input type="radio" name="scaling[]"  value="nf" /> Resize the image to fit within the frame. but pad the image with white to ensure the resolution matches the frame<br />
+					<input type="radio" name="scaling[]"  value="n" /> Resize the image to fit within the frame. but do not upscale the image.<br />
+					<input type="radio" name="scaling[]"  value="nc" /> Resize the image to fit within the frame. Image will not exceed specified dimensions
+				</td>
+			</tr>			
 		
 		</tbody>
 	</table>
