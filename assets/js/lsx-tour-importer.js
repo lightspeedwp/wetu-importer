9f535835c47605ee691af2e7fddbfb0c760991f0
Warwick <warwick@feedmymedia.com>
2016-06-09 11:51:51 +0200

    Added in the ajax loader

--- assets/js/lsx-tour-importer.js
+++ assets/js/lsx-tour-importer.js
@@ -2,14 +2,14 @@ var LSX_TOUR_IMPORTER = {
 
 	init : function() {
 		if(jQuery('body').hasClass('tools_page_lsx-tour-importer')){
-
 			this.watchTypeDropdown();
+			this.watchSearch();
 		}
 	},
 	
 	watchTypeDropdown: function() {
 		
-		jQuery('select').on( 'change', function() {
+		jQuery('#lsx-tour-importer-search-form select').on( 'change', function() {
 			console.log(jQuery(this).val());
 			if('accommodation' == jQuery(this).val()){
 				jQuery('.tour-options' ).hide();
@@ -18,6 +18,27 @@ var LSX_TOUR_IMPORTER = {
 			}
 		});	
 	},	
+	watchSearch: function() {
+		jQuery('#lsx-tour-importer-search-form').on( 'submit', function(event) {
+			event.preventDefault();
+
+			jQuery('#the-list').html(jQuery('#lsx-tour-importer-search-form .ajax-loader').html());
+
+			var type = jQuery('#lsx-tour-importer-search-form select.content-type[selected="selected"]').val();
+			var keyword = jQuery('#lsx-tour-importer-search-form input.keyword').val();
+			jQuery.post(lsx_tour_importer_params.ajax_url,
+	        {
+	            'action' 	: 			'lsx_tour_importer',
+	            'type'		: 			type,
+	            'keyword' 	: 			keyword
+	        },
+	        function(response) {
+	        	jQuery('#the-list').html(response);
+	        });
+			return false;
+		});	
+	},	
+		
 }
 
 jQuery(document).ready( function() {
--- lsx-tour-importer.php
+++ lsx-tour-importer.php
@@ -107,6 +107,8 @@ class Lsx_Tour_Importer {
 		add_action( 'admin_menu', array( $this, 'register_importer_page' ) );
 		add_action( 'admin_enqueue_scripts', array($this,'admin_scripts') ,11 );
 
+		add_action('wp_ajax_lsx_tour_importer',array($this,'process_ajax_search'));	
+		add_action('wp_ajax_nopriv_lsx_tour_importer',array($this,'process_ajax_search'));
 	}	
 
 	/**
@@ -192,12 +194,8 @@ class Lsx_Tour_Importer {
         ?>
         <div class="wrap">
             <?php screen_icon(); ?>
-            <form id="<?php $this->plugin_slug; ?>-import-form" method="post" action="tools.php">
-            	<input type="hidden" name="page" value="<?php $this->plugin_slug; ?>" />
-
-            	<?php echo $this->image_scaling_url; ?><br />
-            	<?php echo $this->list_url; ?><br />
-            	<?php echo $this->import_url; ?><br />
+            <form id="<?php echo $this->plugin_slug; ?>-search-form" method="get" action="tools.php">
+            	<input type="text" name="page" value="<?php echo $this->plugin_slug; ?>" />
 
             	<h3><?php _e('Settings','lsx-tour-importer'); ?></h3>
 
@@ -219,11 +217,65 @@ class Lsx_Tour_Importer {
 
             	<h3><?php _e('Search','lsx-tour-importer'); ?></h3>
             	<p>
-            		<input placeholder="Enter the title you are searching for" class="keyword" name="keyword" value="">
+            		<input pattern=".{3,}" placeholder="3 characters minimum" class="keyword" name="keyword" value=""> <input class="submit" type="submit" value="<?php _e('Search','lsx-tour-importer'); ?>" />
             	</p>
 
+	            <div class="ajax-loader" style="display:none;width:100%;text-align:center;width:64px;">
+	            	<img src="<?php echo LSX_TOUR_IMPORTER_URL.'assets/images/ajaxloader.gif';?>" />
+	            </div>
             	
             </form>
+
+
+
+
+			<form method="get" action="" id="posts-filter">
+				<input type="hidden" name="post_type" class="post_type" value="<?php echo $post_type; ?>" />
+				
+				<table class="wp-list-table widefat fixed posts">
+					<thead>
+						<tr>
+							<th style="" class="manage-column column-cb check-column" id="cb" scope="col">
+								<label for="cb-select-all-1" class="screen-reader-text">Select All</label>
+								<input type="checkbox" id="cb-select-all-1">
+							</th>
+							<th style="" class="manage-column column-title sortable desc" id="title" style="width:50%;" scope="col">
+								<span>Title</span><span class="sorting-indicator"></span>
+							</th>
+							<th style="" class="manage-column column-date sortable asc" id="date" scope="col">
+									<span>Date</span><span class="sorting-indicator"></span>
+							</th>
+							<th style="" class="manage-column column-ssid" id="ssid" scope="col">ID</th>
+						</tr>
+					</thead>
+				
+					<tbody id="the-list">
+						<tr class="post-0 type-tour status-none" id="post-0">
+							<th class="check-column" scope="row">
+								<label for="cb-select-0" class="screen-reader-text"><?php _e('Enter a title to search for and press enter','lsx-tour-importer'); ?></label>
+							</th>
+							<td class="post-title page-title column-title">
+								<strong>
+									<?php _e('Enter a title to search for','lsx-tour-importer'); ?>
+								</strong>
+							</td>
+							<td class="date column-date">							
+							</td>
+							<td class="ssid column-ssid">
+							</td>
+						</tr>									
+					</tbody>
+
+					<tfoot>
+						<tr>
+							<th style="" class="manage-column column-cb check-column" scope="col"><label for="cb-select-all-2" class="screen-reader-text">Select All</label><input type="checkbox" id="cb-select-all-2"></th>
+							<th style="" class="manage-column column-title sortable desc" scope="col"><a href="http://localhost.localdomain/fmm/asc/wp-admin/edit.php?post_type=tour&amp;orderby=title&amp;order=asc"><span>Title</span><span class="sorting-indicator"></span></a></th>
+							<th style="" class="manage-column column-date sortable asc" scope="col"><a href="http://localhost.localdomain/fmm/asc/wp-admin/edit.php?post_type=tour&amp;orderby=date&amp;order=desc"><span>Date</span><span class="sorting-indicator"></span></a></th>
+							<th style="" class="manage-column column-ssid" scope="col">ID</th>
+						</tr>
+					</tfoot>
+				</table>
+			</form>          
         </div>
         <?php		
 	}		
@@ -233,17 +285,40 @@ class Lsx_Tour_Importer {
 	 */
 	public function admin_scripts() {
 		if(is_admin() && isset($_GET['page']) && $this->plugin_slug === $_GET['page']){
-			wp_enqueue_script( 'wetu-importer-admin-script', LSX_TOUR_IMPORTER_URL.'assets/js/lsx-tour-importer.js');
+			wp_enqueue_script( 'lsx-tour-importers-script', LSX_TOUR_IMPORTER_URL.'assets/js/lsx-tour-importer.js');
+			wp_localize_script( 'lsx-tour-importers-script', 'lsx_tour_importer_params', array(
+			'ajax_url' => admin_url('admin-ajax.php'),
+			) );			
 		}
 	}	
 
 	/**
-	 * Display the search form for the admin screen
+	 * Connect to wetu
 	 */
-	public function search_form() {
+	public function process_ajax_search() {
+		if(isset($_POST['action']) && $_POST['action'] == $this->plugin_slug){
 
+			
+			$data= file_get_contents($this->list_url);
+			$all_data  = json_decode($data, true);
+
+			print_r($_GET['keyword']);
+
+			if(isset($_GET['keyword'])){
+				$searched_items = false;
+				$search_keyword = urldecode($_GET['keyword']);
+				if (!empty($all_data)) {
+					foreach($all_data as $row_key => $row){
+						if(stripos($row->name, $search_keyword) !== false){
+							$searched_items[] = $row;
+						}
+					}		
+				}	
+			}
+			print_r($searched_items);
+		}	
+		die();
 	}
-
 
 
 }
