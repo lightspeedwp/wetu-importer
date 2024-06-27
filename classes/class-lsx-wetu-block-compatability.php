f8bfe565da1d060330a7e31f774d97d2bbc89127
Warwick <krugazul@gmail.com>
2024-06-27 13:27:29 +0200

    Updating the block functions

--- classes/class-lsx-wetu-block-compatability.php
+++ classes/class-lsx-wetu-block-compatability.php
@@ -5,8 +5,11 @@ add_action( 'init', 'lsx_wetu_register_meta' );
 function lsx_wetu_register_meta() {
 
 	$fields = array(
+		//Not needed
 		'included'     => array(),
 		'not_included' => array(),
+
+		//Search.
 		'duration'     => array(),
 		'price'        => array(),
 	);
@@ -44,6 +47,7 @@ add_action( 'init', 'lsx_wetu_register_block_bindings' );
 
 function lsx_wetu_bindings_callback( $source_args, $block_instance ) {
 	if ( 'core/image' === $block_instance->parsed_block['blockName'] ) {
+
 		return 'test_image';
 	} elseif ( 'core/paragraph' === $block_instance->parsed_block['blockName'] ) {
 		$value = get_post_meta( get_the_ID(), $source_args['key'], true );
@@ -54,4 +58,32 @@ function lsx_wetu_bindings_callback( $source_args, $block_instance ) {
 
 		return $value;
 	}
+}
+
+//add_filter( 'render_block', 'lsx_wetu_render_block', 10, 3 );
+function lsx_wetu_render_block( $block_content, $parsed_block, $block_obj ) {
+	// Determine if this is the custom block variation.
+	if ( ! isset( $parsed_block['blockName'] ) || ! isset( $parsed_block['attrs'] )  ) {
+		return $block_content;
+	}
+	$allowed_blocks = array(
+		'core/paragraph'
+	);
+	$allowed_sources = array(
+		'core/post-meta',
+		'lsx/post-connection'
+	);
+	if ( ! in_array( $parsed_block['blockName'], $allowed_blocks, true ) ) {
+		return $block_content; 
+	}
+
+	if ( ! isset( $parsed_block['attrs']['metadata']['bindings']['content']['source'] ) ) {
+		return $block_content;
+	}
+
+	if ( ! in_array( $parsed_block['attrs']['metadata']['bindings']['content']['source'], $allowed_sources ) ) {
+		return $block_content;
+	}
+
+	return $block_content;
 }
