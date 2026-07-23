793ea5536b06c6abc3dc5530b23ed676eaad69f7
krugazul <krugazul@gmail.com>
2026-07-23 16:26:00 +0200

    Fix condition to check for tour title option in post update

--- classes/class-lsx-wetu-importer-tours.php
+++ classes/class-lsx-wetu-importer-tours.php
@@ -562,7 +562,7 @@ class LSX_WETU_Importer_Tours extends LSX_WETU_Importer {
 		if ( false !== $id && '0' !== $id ) {
 			$post['ID']          = $id;
 			$post['post_status'] = 'publish';
-			if ( isset( $this->options ) && 'on' !== $this->options['disable_accommodation_title'] ) {
+			if ( isset( $this->options ) && 'on' !== $this->options['disable_tour_title'] ) {
 				$post['post_title'] = $data['name'];
 			}
 			$id = wp_update_post( $post );
